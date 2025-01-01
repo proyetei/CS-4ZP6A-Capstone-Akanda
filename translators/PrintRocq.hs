b97fb9fdebe641cb86aa25855f8153d9ee6cd9b8
Z-Obaid <82549386+Z-Obaid@users.noreply.github.com>
2025-01-01 17:32:48 -0500

    Nested Function
    Created Nested function test under main (incomplete). Created FunCall under grammar to enable function calling within expression.

---
+++ translators/PrintRocq.hs
@@ -0,0 +1,35 @@
+module PrintRocq (runRocq) where
+
+import Grammar
+import Data.Char
+
+imports = ""
+
+printRocq :: Module -> String
+printRocq (Module name defs) =
+    let
+        headers = "Module " ++ name ++ ".\n" ++ imports
+        printType (Con t) = map toLower t
+        printType (Arr t1 t2) = printType t1 ++ " -> " ++ printType t2
+        printArg a = "(" ++ (arg a) ++ " : " ++ (printType $ ty a) ++ ")"
+        printExpr (Var var) = var
+        printExpr (Int int) = show int
+        printExpr (Bool bool) = show bool
+        printExpr (String str) = str
+        printExpr (Mon op e) = "(" ++ op ++ printExpr e ++ ")"
+        printExpr (Bin op e1 e2) = printExpr e1 ++ op ++ printExpr e2
+        printExpr (Let [] expr) = printExpr expr -- this should never happen
+        printExpr (Let (d:[]) expr) = "let " ++ (printDef d) ++ " in \n\t" ++ printExpr expr
+        printExpr (Let (d:ds) expr) = "let \n\t" ++ (foldr (\x y -> x ++ "\n\t" ++ y) (printDef d) $ map printDef ds) ++ "\n\tin \n\t" ++ printExpr expr -- probably need to recursively indent blocks to make sure everything stays aligned
+        printExpr (If cond thn els) = "if " ++ printExpr cond ++ " then " ++ printExpr thn ++ " else " ++ printExpr els
+        printExpr (Where expr ds) = (++) (printExpr expr) $ (++) "\n\twhere " $ foldr (\x y -> x ++ "\n\t" ++ y) "" $ map printDef ds
+        printDef (DefVar var Nothing expr) = var ++ " := " ++ printExpr expr 
+        printDef (DefVar var (Just t) expr) = "Definition " ++ var ++ " : " ++ printType t ++ " := " ++ printExpr expr ++ ". \nCompute " ++ var ++ "."
+        printDef (DefFun var Nothing args expr) = var ++ (foldr (\x y -> x ++ " " ++ y) "" $ map arg args) ++ " := " ++ printExpr expr 
+        printDef (DefFun var (Just t) args expr) = "Definition " ++ var ++ (foldr (\x y -> x ++ " " ++ y) "" $ map printArg args) ++ " : " ++ printType t ++ " := " ++ printExpr expr ++ "."
+        body = foldr (\x y -> x ++ "\n" ++ y) "" $ map printDef defs
+    in headers ++ "\n" ++ body ++ "\nEnd " ++ name ++ "."
+
+runRocq :: Module -> IO()
+runRocq (Module name d) = do
+    writeFile ("out/" ++ name ++ ".v") $ printRocq (Module name d)
--- translators/main.hs
+++ translators/main.hs
@@ -7,7 +7,7 @@ import PrintLean
 import PrintRocq
 
 -- this is equivalent to:
--- n :: Int
+-- n :: Nat
 -- n = let x1 = 1 in
 --     let x2 = x1 in
 --     let x3 = x2 in
@@ -17,6 +17,27 @@ test = Module "LetExample"
     [
         DefVar "n" (Just $ Con "Nat") (Let [DefVar "x1" Nothing $ Int 1] $ Let [DefVar "x2" Nothing $ Var "x1"] $ Let [DefVar "x3" Nothing $ Var "x2"] $ Var "x3")
     ]
+
+-- this is equivalent to:
+-- n :: Nat
+-- n = let f1 x1 = x1 + 1
+--         f2 x1 x2 = x1 + x2 + 1
+--         f3 x1 x2 x3 = x1 + x2 + x3 + 1
+--     in f1 2 + f2 2 3 + f3 2 3 4
+
+-- test1 :: Module
+-- test1 = Module "NestedFunction"
+--     [
+--         DefVar "n" (Just $ Con "Nat") (Let 
+--             [
+--                 DefFun "f1" Nothing [Arg "x1" (Con "Nat")] (Bin "+" (Var "x1") (Int 1)),
+--                 DefFun "f2" Nothing [Arg "x1" (Con "Nat"), Arg "x2" (Con "Nat")] (Bin "+" (Bin "+" (Var "x2") (Var "x1")) (Int 1))
+--             ] $ Bin "+" ((Var "x1") (Int 2)) (Mon "f2" ) ) 
+--     ] 
+
+-- DefFun "f2" Nothing [Arg "x1" Nothing, Arg "x2" Nothing] (Bin "+" (Bin "+" (Var "x1") (Var "x2") (Int 1)))
+    
+
 
 main :: IO()
 main = do
