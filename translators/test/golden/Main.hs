93684fa004bfc6fde5f30ca7d78132d7c2909e64
Reed Mullanix <reedmullanix@gmail.com>
2025-06-12 16:43:23 -0400

    tests: port tests from Test.hs
    Unfortunately, we don't have sample files from good_Output
    for all tests, so I've disabled the ones with missing outputs
    for now.

--- translators/test/golden/Main.hs
+++ translators/test/golden/Main.hs
@@ -87,7 +87,37 @@ printModuleTestGroup groupName base syn =
 -- so there is a throughline.
 
 main :: IO ()
-main = defaultMain tests
+main = defaultMain $
+  testGroup "Golden"
+  [ printModuleTestGroup "Sequential let bindings" "LetExample" (tests IntMap.! 1 $ 5)
 
-tests :: TestTree
-tests = testGroup "Golden" []
+-- Disabled, as there are no pre-existing snapshot files.
+  -- , printModuleTestGroup "Repeated addition and let Bindings" "LetAddExample" (tests IntMap.! 2 $ 5)
+  -- , printModuleTestGroup "Nested function declarations" "NestedFunction" (tests IntMap.! 3 $ 5)
+  -- , printModuleTestGroup "Simple data declarations" "DataSimpleDeclarations" (tests IntMap.! 4 $ 5)
+  -- , printModuleTestGroup "Single long identifier" "LongIdentifier" (tests IntMap.! 5 $ 5)
+
+  , printModuleTestGroup "Record with lots of dependent fields" "Fields_DependentRecordModule" (tests IntMap.! 6 $ 5)
+  , printModuleTestGroup "Sequence of dependent record declarations" "ChainDef_DependentRecordModule" (tests IntMap.! 7 $ 5)
+  , printModuleTestGroup "Record with lots of parameters" "Parameters_DependentRecordModule" (tests IntMap.! 8 $ 5)
+
+-- Disabled, as there are no pre-existing snapshot files.
+  -- , printModuleTestGroup "File with lots of newlines" "NewlineFile" (tests IntMap.! 9 $ 5)
+  , printModuleTestGroup "Record declaration with non-dependent fields" "Fields_NonDependentRecordModule" (tests IntMap.! 10 $ 5)
+  , printModuleTestGroup "Sequence of non-dependent record declarations" "ChainDefFields_NonDependentRecordModule" (tests IntMap.! 11 $ 5)
+
+-- Disabled, as there are no pre-existing snapshot files.
+  -- , printModuleTestGroup "Non indexed, non parameterised datatype with many constructors" "Constructors_Datatypes" (tests IntMap.! 12 $ 5)
+  -- , printModuleTestGroup "Non indexed, parameterised datatype with many constructors" "Parameters_Datatypes" (tests IntMap.! 13 $ 5)
+
+  , printModuleTestGroup "Lots of definitions" "FirstLast_VariableModule" (tests IntMap.! 14 $ 5)
+  , printModuleTestGroup "Lots of dependent definitions" "DeepDependency_VariableModule" (tests IntMap.! 15 $ 5)
+
+-- Disabled, as there are no pre-existing snapshot files.
+  -- , printModuleTestGroup "Datatype with lots of indicies" "DataImplicitIndices" (tests IntMap.! 16 $ 5)
+  -- , printModuleTestGroup "A file with a single long line" "SingleLongLine" (tests IntMap.! 17 $ 5)
+  -- , printModuleTestGroup "A datatype with lots of 'Type'-valued parameters" "ConstructorsParameters_Datatypes" (tests IntMap.! 18 $ 5)
+  -- , printModuleTestGroup "A datatype with a lot of indices that are used in constructors" "IndicesConstructors_Datatypes" (tests IntMap.! 19 $ 5)
+  -- , printModuleTestGroup "A datatype with lots of parameters and indices" "IndicesParameters_Datatypes" (tests IntMap.! 20 $ 5)
+  -- , printModuleTestGroup "A pattern matching function with lots of cases" "Pattern_Matching_Datatypes" (tests IntMap.! 21 $ 5)
+  ]
